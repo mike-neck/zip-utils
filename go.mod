0699354212014fdad5d13516651030f82ef74062
mike-neck <jkrt3333@gmail.com>
2024-04-02 15:37:17 +0900

    Update list/main.go to add display a hash of name, update date, index.
    This update introduces functionality to display a hash of raw string (name), update date, and index in list/main.go. This is designed to provide necessary information for file identification through hash, even when complete resolution of garbling cannot be achieved.

--- go.mod
+++ go.mod
@@ -2,4 +2,13 @@ module github.com/mike-neck/zip-utils
 
 go 1.22.1
 
-require golang.org/x/text v0.14.0
+require (
+	github.com/urfave/cli/v2 v2.27.1
+	golang.org/x/text v0.14.0
+)
+
+require (
+	github.com/cpuguy83/go-md2man/v2 v2.0.2 // indirect
+	github.com/russross/blackfriday/v2 v2.1.0 // indirect
+	github.com/xrash/smetrics v0.0.0-20201216005158-039620a65673 // indirect
+)
--- list/main.go
+++ list/main.go
@@ -4,33 +4,117 @@ import (
 	"archive/zip"
 	"fmt"
 	"github.com/mike-neck/zip-utils"
-	"log"
+	"github.com/urfave/cli/v2"
+	"hash/fnv"
 	"os"
+	"strings"
 )
 
 func main() {
-	if len(os.Args) == 1 {
-		_, _ = fmt.Fprintln(os.Stderr, "no file specified")
-		os.Exit(1)
-	} else if 2 < len(os.Args) {
-		_, _ = fmt.Fprintln(os.Stderr, "only a single file is acceptable.")
-		os.Exit(2)
+	// list-zip コマンド
+	// github.com/urfave/cli/v2 を使ってコマンドラインアプリケーションを組み立てる
+	// オプション・オプションなし引数を使って ListOption 構造体を組み立てる
+	// オプション指定なしのパラメーターを ListOption の FilePath(必須)に指定する(For implementation Do not use cli.StringFlag)
+	// -r/--raw-string オプションが ListOption の RawString(指定無しの場合は falseで、ファイルの名前を ShiftJIS->UTF-8 に変換して表示する)
+	// -s/--show-hash オプションが ListOption の ShowHash(指定無しの場合は false)
+	// 組み立てられた ListOption の listZipFileEntries() 関数を呼び出すことがアプリケーション本体の内容
+	// 上記の仕様を満たすプログラムを書いてください。
+	rawString := false
+	showHash := false
+	app := &cli.App{
+		Name:  "list-zip",
+		Usage: "List the entries in a zip file",
+		Flags: []cli.Flag{
+			&cli.BoolFlag{
+				Name:        "raw-string",
+				Aliases:     []string{"r"},
+				Usage:       "Display file names in raw string format (default false: converts file names from ShiftJIS to UTF-8)",
+				Destination: &rawString,
+			},
+			&cli.BoolFlag{
+				Name:        "show-hash",
+				Aliases:     []string{"s"},
+				Usage:       "Display hash value of each entry",
+				Destination: &showHash,
+			},
+		},
+		Action: func(c *cli.Context) error {
+			lo := ListOption{
+				FilePath:  c.Args().First(),
+				RawString: rawString,
+				ShowHash:  showHash,
+			}
+			return lo.listZipFileEntries()
+		},
 	}
 
-	// zipファイルを開く
-	r, err := zip.OpenReader(os.Args[1])
+	err := app.Run(os.Args)
 	if err != nil {
-		log.Fatal(err)
+		_, _ = fmt.Fprintln(os.Stderr, err)
+		os.Exit(3)
+	}
+}
+
+type ListOption struct {
+	FilePath  string
+	RawString bool
+	ShowHash  bool
+}
+
+func (lo ListOption) String() string {
+	return fmt.Sprintf("ListOption[FilePath: %s, RawString: %v, ShowHash: %v]", lo.FilePath, lo.RawString, lo.ShowHash)
+}
+
+func (lo ListOption) listZipFileEntries() error {
+	r, err := zip.OpenReader(lo.FilePath)
+	if err != nil {
+		return err
 	}
 	defer r.Close()
 
 	// エントリーの一覧を取得して表示する
-	for _, f := range r.File {
+	es := make([]string, 0)
+	for i, f := range r.File {
 		// エントリーのファイル名をShiftJISからUTF-8に変換する
-		filename, err := charsets.SJISToUtf8(f.Name)
+		filename, err := lo.ExtractFileName(f.Name)
 		if err != nil {
-			log.Fatal(err)
+			es = append(es, fmt.Sprintf("    %s", err))
+		} else {
+			outputs := lo.MakePrintFormat(i, filename, f.FileHeader)
+			fmt.Println(outputs)
 		}
-		fmt.Println(filename)
 	}
+	if len(es) > 0 {
+		all := strings.Join(es, "\n")
+		return fmt.Errorf("errors occurred during conversion: \n%s", all)
+	}
+	return nil
+}
+
+func (lo ListOption) ExtractFileName(name string) (string, error) {
+	if lo.RawString {
+		return name, nil
+	} else {
+		return charsets.SJISToUtf8(name)
+	}
+}
+
+func (lo ListOption) MakePrintFormat(index int, name string, f zip.FileHeader) string {
+	outputs := make([]string, 0)
+	if lo.ShowHash {
+		hash := CalculateHash(index, f)
+		outputs = append(outputs, fmt.Sprintf("%08x", hash))
+	}
+	outputs = append(outputs, name)
+	return strings.Join(outputs, " ")
+}
+
+func CalculateHash(index int, f zip.FileHeader) uint32 {
+	hf := fnv.New32a()
+	// time.Time 型の f.Modified を文字列にする
+	modified := f.Modified.Format("2006-01-02T15:04:05")
+	entry := fmt.Sprintf("%04d:%-60s:%s", index, f.Name, modified)
+	_, _ = hf.Write([]byte(entry))
+	hash := hf.Sum32()
+	return hash
 }
